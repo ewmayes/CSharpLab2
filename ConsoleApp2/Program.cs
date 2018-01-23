5b7c650f3c5150a913a7b047b3d4af0deb6c1342
Everett Mayes <ewmayes@hotmail.com>
2018-01-23 13:45:56 -0500

    This is the first commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input
+            Console.WriteLine("Enter a number between 1 and 100:");
+            int Number = Convert.ToInt32(Console.ReadLine());
+
+
+            // Processing
+            if (Number % 2 != 0)
+                Console.WriteLine("Odd.");
+
+            else if (Number < 26)
+                Console.WriteLine("Even and less than 25.");
+
+            else if (Number < 61)
+                Console.WriteLine("Even.");
+
+            else
+                Console.WriteLine("Even.");
+        }
+    }
+}
